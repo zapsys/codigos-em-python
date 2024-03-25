18f09957eda886b76df3e0b81023ec0674d0b9ca
zapsys <zapsyssolutions@gmail.com>
2024-03-25 11:57:32 -0300

    Exemplo de definição de uma função sem parâmetros

---
+++ exemplo_funcao.py
@@ -0,0 +1,13 @@
+# Uso de uma função com input para retornar as informações
+def info():
+    nome_cliente = input("Nome do cliente: ")
+    nome_lugar = input("Nome do lugar: ")
+    dia = int(input("Dia da semana: "))
+    mes = int(input("Mês: "))
+    ano = int(input("Ano: "))
+
+    return print(f"\nInformações:\n{nome_cliente}\n{nome_lugar}\n{dia}/{mes}/{ano}\n")
+
+
+# Chamando a função definida acima
+info()
