3bb4dce226c8547c68e7d46ee220b38ed051c84d
zapsys <zapsyssolutions@gmail.com>
2024-04-04 17:45:32 -0300

    Substituído aspas duplas por simples

--- exemplo_funcao.py
+++ exemplo_funcao.py
@@ -1,12 +1,12 @@
 # Uso de uma função com input para retornar as informações
 def info():
-    nome_cliente = input("Nome do cliente: ")
-    nome_lugar = input("Nome do lugar: ")
-    dia = int(input("Dia da semana: "))
-    mes = int(input("Mês: "))
-    ano = int(input("Ano: "))
+    nome_cliente = input('Nome do cliente: ')
+    nome_lugar = input('Nome do lugar: ')
+    dia = int(input('Dia da semana: '))
+    mes = int(input('Mês: '))
+    ano = int(input('Ano: '))
 
-    return print(f"\nInformações:\n{nome_cliente}\n{nome_lugar}\n{dia}/{mes}/{ano}\n")
+    return print(f'\nInformações:\n{nome_cliente}\n{nome_lugar}\n{dia}/{mes}/{ano}\n')
 
 
 # Chamando a função definida acima
