1015ebcb3179059374ab963eb34ad932c8e6acf0
zapsys <zapsyssolutions@gmail.com>
2024-04-05 16:36:31 -0300

    Algoritmo que imprime os 10 primeiros termos da sequência de Fibonacci

---
+++ sequencia_fibonacci.py
@@ -0,0 +1,14 @@
+# Algoritmo que imprime os 10 primeiros termos da sequência de Fibonacci
+# Valores a serem impressos: 1 1 2 3 5 8 13 21 34 55
+
+cont = 0  # Variável contadora do laço de repetição
+n_atual = 1  # Variável que armazena o termo atual da sequência (inicia com 1)
+n_anter = 1  # Variável que armazena o termo anterior da sequência (inicia com 1)
+soma = 1  # Variável que armazena a soma do termo atual + anterior
+
+while cont < 10:
+    n_anter = n_atual
+    n_atual = soma
+    cont += 1  # Incrementa o contador a cada repetição
+    soma = n_atual + n_anter
+    print(n_anter)
