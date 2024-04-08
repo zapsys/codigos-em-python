66316e768de1f90e1a3a1e1c5fcb9d4cd1281481
zapsys <zapsyssolutions@gmail.com>
2024-04-08 15:30:45 -0300

    Comentários de variáveis atualizados

--- exemplo_vetor_while.py
+++ exemplo_vetor_while.py
@@ -2,12 +2,12 @@
 # Um vetor é um conjunto de valores, do mesmo tipo.
 # Pode-se se ter vetores do tipo linha ou coluna
 
-A = [None] * 10  # vetor A vazio com 10 elementos.
+A = [None] * 10  # Vetor A com 10 posiçõs sem elementos definidos
 
 i = 0  # contador
 
 # Vetor linha com 10 elementos (0 a 9)
 while i < 10:
     A[i] = i
-    i += 1  # Incrementa 1 ao valor de i
+    i += 1  # Incrementa 1 ao valor de i a cada repetição
 print(A)
